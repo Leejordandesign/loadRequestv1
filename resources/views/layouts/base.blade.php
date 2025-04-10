23c0473ac7bb7067a94afe2e6f0668c95fa32273
Leejordandesign <jordan@leejordandesign.com>
2025-04-10 17:13:57 -0400

    getting started

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html class="h-full bg-gray-300">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>@yield('pageTitle')</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="h-full">
+
+<div class="min-h-full">
+    <nav class="bg-gray-800">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 items-center justify-center">
+                <div class="flex items-center">
+                    <div class="shrink-0">
+{{--                        <img class="size-8" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">--}}
+                        <img class="size-8" src="{{\Illuminate\Support\Facades\URL::asset('images/flower_flat_white.png')}}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <header class="bg-white shadow-sm">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl text-center font-bold tracking-tight text-gray-900">@yield('pageTitle')</h1>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+            @yield('content')
+        </div>
+    </main>
+</div>
+
+
+</body>
+</html>
